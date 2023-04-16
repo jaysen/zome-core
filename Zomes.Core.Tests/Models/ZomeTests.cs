ffea8f6eaa08d260cfb4a975572fee9b8dfa011e
Jaysen Naidoo <jaysen@gmail.com>
2023-04-16 21:13:07 +0200

    chatGPT generated tests for ZomeTests

--- Zomes.Core.Tests/Models/ZomeTests.cs
+++ Zomes.Core.Tests/Models/ZomeTests.cs
@@ -14,5 +14,146 @@ public class ZomeTests
         zome.Tags.Should().BeEmpty();
     }
 
-    // Add additional test cases for Zome class here
+    [Fact]
+    public void AddNode_AddsNodeToZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var node = new Node("Example");
+
+        // Act
+        zome.AddNode(node);
+
+        // Assert
+        zome.Nodes.Should().Contain(node);
+    }
+
+    [Fact]
+    public void RemoveNode_RemovesNodeFromZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var node = new Node("Example");
+        zome.AddNode(node);
+
+        // Act
+        zome.RemoveNode(node);
+
+        // Assert
+        zome.Nodes.Should().NotContain(node);
+    }
+
+    [Fact]
+    public void HasNode_ReturnsTrue_WhenNodeExistsInZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var node = new Node("Example");
+        zome.AddNode(node);
+
+        // Act
+        var hasNode = zome.HasNode(node);
+
+        // Assert
+        hasNode.Should().BeTrue();
+    }
+
+    [Fact]
+    public void AddLink_AddsLinkToZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var source = new Node("Source");
+        var target = new Node("Target");
+        var link = new Link(source, target);
+
+        // Act
+        zome.AddLink(link);
+
+        // Assert
+        zome.Links.Should().Contain(link);
+    }
+
+    [Fact]
+    public void RemoveLink_RemovesLinkFromZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var source = new Node("Source");
+        var target = new Node("Target");
+        var link = new Link(source, target);
+        zome.AddLink(link);
+
+        // Act
+        zome.RemoveLink(link);
+
+        // Assert
+        zome.Links.Should().NotContain(link);
+    }
+
+    [Fact]
+    public void HasLink_ReturnsTrue_WhenLinkExistsInZome()
+    {
+        // Arrange
+        var zome = new Zome();
+        var source = new Node("Source");
+        var target = new Node("Target");
+        var link = new Link(source, target);
+        zome.AddLink(link);
+
+        // Act
+        var hasLink = zome.HasLink(link);
+
+        // Assert
+        hasLink.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsConnected_ReturnsTrue_WhenNodesAreConnected()
+    {
+        // Arrange
+        var zome = new Zome();
+        var node1 = new Node("Node1");
+        var node2 = new Node("Node2");
+        var link = new Link(node1, node2);
+        zome.AddLink(link);
+
+        // Act
+        var isConnected = zome.IsConnected(node1, node2);
+
+        // Assert
+        isConnected.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetNeighbors_ReturnsListOfNeighbors()
+    {
+        // Arrange
+        var zome = new Zome();
+        var node1 = new Node("Node1");
+        var node2 = new Node("Node2");
+        var node3 = new Node("Node3");
+        var link1 = new Link(node1, node2);
+        var link2 = new Link(node1, node3);
+        zome.AddLink(link1);
+        zome.AddLink(link2);
+
+        // Act
+        var neighbors = zome.GetNeighbors(node1);
+
+        // Assert
+        neighbors.Should().HaveCount(2).And.Contain(new[] { node2, node3 });
+    }
+
+    // Add more test cases for Zome class here, including tests for:
+    // - GetLink
+    // - GetLinks
+    // - GetNodesWithTag
+    // - GetLinksWithTag
+    // - FindShortestPath
+    // - FindWeightedShortestPath
+    // - AddTag
+    // - RemoveTag
+    // - HasTag
+    // - GetAllTags
 }
